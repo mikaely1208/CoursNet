5a9f99465723daefaa29a5417e63de22c5209d00
mikaely1208 <mikaely.andrianandraina@ecole-isitech.fr>
2023-12-05 13:40:33 +0100

    init controller

--- webapi/api/Models/Book.cs
+++ webapi/api/Models/Book.cs
@@ -1,14 +1,38 @@
 namespace webapi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 public class Book
 {
+    [Key]
     public int Id { get; set; }
-    public string? Title { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(50)")]
+    [StringLength(50)]
+    public string? Title { get; set; } = null!;
+
+
+    [Column(TypeName = "ntext")]
     public string? Author { get; set; }
+
+    [Column(TypeName = "nvarchar(20)")]
+    [StringLength(20)]
     public string? Genre { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")] //La précision = référence au nombre total de chiffres qu'on peut stocker dans la colonne échelle =  nombre de décimales
     public decimal Price { get; set; }
+
+    [Column(TypeName = "datetime")]
     public DateTime PublishDate { get; set; }
+
+    [Column(TypeName = "ntext")]
     public string? Description { get; set; }
+
+    [Column(TypeName = "ntext")]
     public string? Remarks { get; set; }
 
 }
